0162ade5c8398577121989709b5b2eb942bd4281
roy5444546495 <544546495@qq.com>
2025-09-18 20:12:34 +0800

    Add clip cov & clip cov ws sh file

---
+++ examples/tuning/0.5b/qwen2-0.5b_grpo-lora_1_h100_fsdp_vllm_clip_cov_ws.sh
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+NOW=$(date +%Y%m%d)
+export WANDB_DIR=gsm8k-grpo-lora-qwen2.5-0.5b-${NOW}
+export WANDB_PROJECT=${WANDB_DIR}
+export WANDB_EXP=0.5b-${NOW}
+MODEL_PATH=/root/qwen2.5-0.5b-instruct
+
+set -x
+nproc_per_gpu=116
+nnodes=1
+ngpu_per_node=1
+total_procs=$(( nproc_per_gpu * nnodes * ngpu_per_node ))
+mini_batch_size=$(( total_procs ))
+
+# Algorithms
+loss_mode="clip_cov_ws"
+loss_agg_mode="token-mean"
+clip_ratio_low=0.2
+clip_ratio_high=0.2
+clip_ratio_c=3
+clip_cov_ratio=0.0002
+clip_cov_lb=1.0
+clip_cov_ub=5.0
+
+python3 -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=grpo \
+    data.train_files=/root/OpenVerl/verl/math/train.parquet \
+    data.val_files=/root/OpenVerl/verl/math/test.parquet \
+    data.train_batch_size=${total_procs} \
+    data.val_batch_size=${total_procs} \
+    data.max_prompt_length=512 \
+    data.max_response_length=1024 \
+    data.filter_overlong_prompts=True \
+    data.truncation='error' \
+    data.shuffle=False \
+    actor_rollout_ref.model.path=$MODEL_PATH  \
+    actor_rollout_ref.model.use_shm=True  \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.model.lora_rank=32 \
+    actor_rollout_ref.model.lora_alpha=32 \
+    actor_rollout_ref.model.target_modules=all-linear \
+    actor_rollout_ref.actor.optim.lr=3e-5 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.ppo_mini_batch_size=${mini_batch_size} \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=${mini_batch_size} \
+    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.actor.policy_loss.loss_mode=${loss_mode} \
+    actor_rollout_ref.actor.clip_ratio_low=${clip_ratio_low} \
+    actor_rollout_ref.actor.clip_ratio_high=${clip_ratio_high} \
+    actor_rollout_ref.actor.clip_ratio_c=${clip_ratio_c} \
+    actor_rollout_ref.actor.policy_loss.clip_cov_ratio=${clip_cov_ratio} \
+    actor_rollout_ref.actor.policy_loss.clip_cov_lb=${clip_cov_lb} \
+    actor_rollout_ref.actor.policy_loss.clip_cov_ub=${clip_cov_ub} \
+    actor_rollout_ref.actor.fsdp_config.fsdp_size=-1 \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size=${mini_batch_size} \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.1 \
+    actor_rollout_ref.rollout.n=5 \
+    actor_rollout_ref.rollout.max_num_seqs=512 \
+    actor_rollout_ref.rollout.max_model_len=1536 \
+    actor_rollout_ref.rollout.max_num_batched_tokens=1536 \
+    actor_rollout_ref.rollout.enable_chunked_prefill=False \
+    actor_rollout_ref.rollout.load_format=safetensors \
+    actor_rollout_ref.rollout.layered_summon=True \
+    actor_rollout_ref.ref.log_prob_micro_batch_size=${mini_batch_size} \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.ulysses_sequence_parallel_size=1 \
+    actor_rollout_ref.actor.entropy_coeff=0.001 \
+    algorithm.kl_ctrl.kl_coef=0.001 \
+    algorithm.use_kl_in_reward=False \
+    trainer.critic_warmup=0 \
+    trainer.logger='["console","wandb"]' \
+    trainer.project_name=${WANDB_PROJECT} \
+    trainer.experiment_name=${WANDB_EXP} \
+    trainer.n_gpus_per_node=1 \
+    trainer.nnodes=1 \
+    trainer.save_freq=20 \
+    trainer.test_freq=5 \
+    trainer.total_epochs=1 $@ 2>&1 | tee ${WANDB_PROJECT}.log
